fa7c2aea882ba6b4c34d02c09dbcef83bd6a5282
artur pereira <arturrocha268@gmail.com>
2025-09-25 09:16:53 -0300

    Update Servico.java

--- Servico.java
+++ Servico.java
@@ -1,1 +1,19 @@
+package cliente;
 
+public abstract class Servico {
+    protected String nome;
+    protected double preco;
+
+    public Servico(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() { return nome; }
+    public double getPreco() { return preco; }
+    
+    @Override
+    public String toString() {
+        return "Serviço: " + nome + ", Preço: R$" + String.format("%.2f", preco);
+    }
+}
